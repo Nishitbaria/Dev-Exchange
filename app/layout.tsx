96cfa42e75e78c8d9270b07d60771739fa348d4e
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-11-21 16:11:10 +0530

    Update layout.tsx with font and metadata changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,45 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "Devflow",
+  description:
+    "Devflow is a platform for developers to share their ideas and projects with others.",
+  icons: {
+    icon: "favicon.ico",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      elements:  {
+        formButtonPrimary : "primary-gradient",
+        footerActionLink: "primary-text-gradient hover:text-primary-500",
+      }
+    }}>
       <html lang="en">
-        <body>{children}</body>
+        <body className={` ${inter.variable} ${spaceGrotesk.variable}  `}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
