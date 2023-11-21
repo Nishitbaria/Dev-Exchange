4d50a15273350e4a8e84d6ddc2253c8b9cafb5c1
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-11-21 14:55:24 +0530

    add : auth in app

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div> Ask the Question</div>
+  )
+}
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <h1 className="h1-bold ">Home</h1>;
-}
