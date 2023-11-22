be183c68e2c00ec03034e2556bf42fc3b3fc16d2
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-11-22 11:32:42 +0530

    Fix formatting and add new dependencies

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,9 @@
-import { UserButton } from "@clerk/nextjs";
+
  
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/"/>
+    Home
     </div>
   )
 }
---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,40 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/assets/images/site-logo.svg"
+          alt="site logo"
+          width={23}
+          height={23}
+        />
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev <span className="text-primary-500">Flow</span>
+        </p>
+      </Link>
+      Global Search
+      <div className="flex-between gap-5  ">
+        Theme
+        <SignedIn>
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: {
+                avatarBox: "h-10 w-10",
+              },
+              variables: {
+                colorPrimary: "#ff7000",
+              },
+            }}
+          />
+        </SignedIn>
+        MobileNav
+      </div>
+    </nav>
+  );
+}
