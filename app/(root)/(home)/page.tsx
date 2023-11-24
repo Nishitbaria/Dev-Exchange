bf2d57f2e3e424cf17b70317ac2231aca0433414
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-11-24 11:55:42 +0530

    Add button and link to home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
- 
 export default function Home() {
   return (
-    <div>
-    Home
-    </div>
-  )
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900  "> All Question </h1>
+
+        <Link href="/question/1" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            {" "}
+            Ask Question{" "}
+          </Button>
+        </Link>
+      </div>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col  sm:items-center">
+        {/* <LocalSearchbar /> */}
+      </div>
+    </>
+  );
 }
