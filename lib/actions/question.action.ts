a279f57c749b8d87c3d16964123be8edccffa7aa
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-11-30 21:24:24 +0530

    Add mdxRs and serverComponentsExternalPackages to next.config.js

---
+++ lib/actions/question.action.ts
@@ -0,0 +1,10 @@
+"use server";
+
+export async function createQuestion(params) {
+  try {
+    // we have to connect to the database
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  mdxRs: true,
+  serverComponentsExternalPackages: ["mongoose"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
