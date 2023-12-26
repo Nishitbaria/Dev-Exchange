fb018939f06c4b054853637c40c73b92d3d2a51e
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-12-26 18:40:02 +0530

    Add console.log statement to log evt.data

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -58,7 +58,7 @@ export async function POST(req: Request) {
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
-
+    console.log(evt.data);
     // Create a new user in your database
     const mongoUser = await createUser({
       clerkId: id,
