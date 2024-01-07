04d2cd5e8ee33163bccf0864370d4ae3aa267811
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-01-07 22:24:27 +0530

    Refactor home page component and update metadata

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -6,66 +6,65 @@ import Pagination from "@/components/shared/Pagination";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
-import {
-  getQuestions,
-  getRecommendedQuestions,
-} from "@/lib/actions/question.action";
+import { getQuestions, getRecommendedQuestions } from "@/lib/actions/question.action";
 import { SearchParamsProps } from "@/types";
 import Link from "next/link";
-import type { Metadata } from "next";
+
+import type { Metadata } from 'next';
 import { auth } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
-  title: "Home",
-  description: "Home Page",
-};
+  title: 'Home | Dev Overflow',
+}
 
 export default async function Home({ searchParams }: SearchParamsProps) {
   const { userId } = auth();
+
   let result;
 
-  if (searchParams?.filter === "recommended") {
-    if (userId) {
+  if(searchParams?.filter === 'recommended') {
+    if(userId) {
       result = await getRecommendedQuestions({
         userId,
         searchQuery: searchParams.q,
         page: searchParams.page ? +searchParams.page : 1,
-      });
+      }); 
     } else {
       result = {
         questions: [],
         isNext: false,
-      };
+      }
     }
   } else {
     result = await getQuestions({
       searchQuery: searchParams.q,
       filter: searchParams.filter,
       page: searchParams.page ? +searchParams.page : 1,
-    });
+    }); 
   }
+  
 
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
-        <h1 className="h1-bold text-dark200_light900  "> All Question </h1>
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1> 
 
         <Link href="/ask-question" className="flex justify-end max-sm:w-full">
-          {" "}
           <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
-            {" "}
-            Ask Question{" "}
+            Ask a Question
           </Button>
-        </Link>
-      </div>
-      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col  sm:items-center">
-        <LocalSearchbar
+        </Link> 
+      </div> 
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar 
           route="/"
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
-          placeholder="Search Globally"
+          placeholder="Search for questions"
           otherClasses="flex-1"
         />
+
         <Filter
           filters={HomePageFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
@@ -74,11 +73,11 @@ export default async function Home({ searchParams }: SearchParamsProps) {
       </div>
 
       <HomeFilters />
+
       <div className="mt-10 flex w-full flex-col gap-6">
-        {/* Loping threw question  */}
-        {result.questions.length > 0 ? (
+        {result.questions.length > 0 ?
           result.questions.map((question) => (
-            <QuestionCard
+            <QuestionCard 
               key={question._id}
               _id={question._id}
               title={question.title}
@@ -90,21 +89,19 @@ export default async function Home({ searchParams }: SearchParamsProps) {
               createdAt={question.createdAt}
             />
           ))
-        ) : (
-          <NoResult
+          : <NoResult 
             title="There’s no question to show"
             description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
             link="/ask-question"
             linkTitle="Ask a Question"
-          />
-        )}
+          />}
       </div>
       <div className="mt-10">
-        <Pagination
+        <Pagination 
           pageNumber={searchParams?.page ? +searchParams.page : 1}
           isNext={result.isNext}
         />
       </div>
     </>
-  );
+  )
 }
