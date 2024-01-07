a6e6e39a2d1706a4628bbf8c7a34238681a1efde
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-01-07 22:47:56 +0530

    Refactor GlobalFilters and GlobalResult components

--- components/shared/search/GlobalFilters .tsx
+++ components/shared/search/GlobalFilters .tsx
@@ -8,7 +8,9 @@ import React, { useState } from "react";
 const GlobalFilters = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
+
   const typeParams = searchParams.get("type");
+
   const [active, setActive] = useState(typeParams || "");
 
   const handleTypeClick = (item: string) => {
@@ -36,20 +38,20 @@ const GlobalFilters = () => {
   };
 
   return (
-    <div className="flex items-center gap-5 px-5 py-2.5">
-      <p className="text-dark400_light900 body-medium "> Tpyes :</p>
-      <div className="flex gap-3 ">
+    <div className="flex items-center gap-5 px-5">
+      <p className="text-dark400_light900 body-medium">Type: </p>
+      <div className="flex gap-3">
         {GlobalSearchFilters.map((item) => (
           <button
             type="button"
             key={item.value}
-            className={`light-border-2 small-medium :text-light-800 rounded-2xl px-5 py-2 capitalize dark:hover:text-primary-500
-           ${
-             active === item.value
-               ? "bg-primary-500 text-light-900"
-               : "bg-light-700 text-dark-400 hover:text-primary-500 dark:bg-dark-500"
-           }
-         `}
+            className={`light-border-2 small-medium :text-light-800 rounded-2xl px-5 py-2 capitalize 
+              ${
+                active === item.value
+                  ? "bg-primary-500 text-light-900"
+                  : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"
+              }
+            `}
             onClick={() => handleTypeClick(item.value)}
           >
             {item.name}
