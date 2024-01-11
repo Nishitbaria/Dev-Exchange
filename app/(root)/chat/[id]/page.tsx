5cddb5eb005d5b59ee9c2b0ff2bb59fab3cb5ee6
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-01-12 00:42:26 +0530

    Remove unused import and empty line in chat page component

--- app/(root)/chat/[id]/page.tsx
+++ app/(root)/chat/[id]/page.tsx
@@ -1,11 +1,10 @@
 import GenerativeAIComponent from "@/components/shared/GenerativeAIComponent";
 import { getUserInfo } from "@/lib/actions/user.action";
 import { URLProps } from "@/types";
-import { auth } from "@clerk/nextjs";
 import React from "react";
 
 const Page = async ({ params, searchParams }: URLProps) => {
-  const { userId: clerkId } = auth();
+  
   const userInfo = await getUserInfo({ userId: params.id });
 
   console.log("userInfo", userInfo.user);
