9b0d12535afa67e10d36a3ed40433d66d0119441
Varad0014 <varadbadhe0014@gmail.com>
2024-05-14 01:56:25 +0530

    Added hover effect to sidebar and filter buttons

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -41,12 +41,11 @@ const HomeFilters = () => {
       {HomePageFilters.map((item) => (
         <Button
           key={item.value}
-          onClick={() => {}}
-          className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
-            active === item.value
-              ? "bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500 dark:hover:bg-dark-400"
-              : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"
-          }`}
+          onClick={() => { }}
+          className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${active === item.value
+              ? "bg-light-700 text-light-500 hover:bg-light-700 dark:bg-dark-400 dark:text-primary-500 dark:hover:bg-dark-400"
+              : "bg-light-800 text-light-500 hover:bg-light-700 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-400"
+            }`}
           onClickCapture={() => handleTypeClick(item.value)}
         >
           {item.name}
