1e839a6aa5425da2029f2ef1721d26f0618de64c
avayyyyyyy <jabhi465@gmail.com>
2024-05-15 22:02:43 +0530

    🐞 fix: Styling Fix

--- components/shared/GenerativeAIComponent.tsx
+++ components/shared/GenerativeAIComponent.tsx
@@ -187,7 +187,7 @@ export default function GenerativeAIComponent({ imgurl, userId }: Props) {
   return (
     <>
       {messages.length > 0 ? (
-        <div className="flex w-full border p-4 text-white  gap-10 mx-[10%] justify-center">
+        <div className="flex w-full p-4 text-white  gap-10 mx-[10%] justify-center">
           {questions.map((e) => (
             <div
               onClick={() => {
