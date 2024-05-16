d02b9b48bc55625b87606314bd13cf175651ebee
Sahil Chalke <sahilchalke@Sahils-MacBook-Air.local>
2024-05-16 19:43:04 +0530

    fixed deployment error of env should be a const

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
-let API_KEY = process.env.NEXT_PUBLIC_GOOGLE_GIMINI_API_KEY;
+const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_GIMINI_API_KEY;
 
 
 const googleAI = new GoogleGenerativeAI(API_KEY);
