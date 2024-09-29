6c4d293c483040eaff309ca07f7c6d9c236bc8a1
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-09-29 18:38:43 +0530

    chore: Refactor Navbar component and add lazy loading for images

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,27 +1,12 @@
-"use client"
-
 import { SignedIn, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
-import React, { useState } from 'react'
-import { useTheme } from '@/context/ThemeProvider'
+import React from 'react'
+import Theme from './Theme'
 import MobileNav from './MobileNav'
 import GlobalSearch from '../search/GlobalSearch'
-import { Button } from "@/components/ui/button"
-import { Moon, Sun, Menu } from 'lucide-react'
 
 const Navbar = () => {
-  const { mode, setMode } = useTheme()
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-  }
-
-  const toggleTheme = () => {
-    setMode(mode === 'light' ? 'dark' : 'light')
-  }
-
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
@@ -31,20 +16,14 @@ const Navbar = () => {
           height={23}
           alt="DevFlow"
         />
+
         <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">Dev <span className="text-primary-500">Exchange</span></p>
       </Link>
 
       <GlobalSearch />
 
       <div className="flex-between gap-5">
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={toggleTheme}
-          className="text-dark-100 dark:text-light-900 hover:bg-primary-500 hover:text-light-900 dark:hover:bg-primary-100 dark:hover:text-dark-100"
-        >
-          {mode === 'light' ? <Moon className="h-5 w-5" /> : <Sun className="h-5 w-5" />}
-        </Button>
+        <Theme />
 
         <SignedIn>
           <UserButton
@@ -60,19 +39,8 @@ const Navbar = () => {
           />
         </SignedIn>
 
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={toggleMenu}
-          className="md:hidden text-dark-100 dark:text-light-900 hover:bg-primary-500 hover:text-light-900 dark:hover:bg-primary-100 dark:hover:text-dark-100"
-        >
-          <Menu className="h-5 w-5" />
-        </Button>
-      </div>
-
-      {isMenuOpen && (
         <MobileNav />
-      )}
+      </div>
     </nav>
   )
 }
